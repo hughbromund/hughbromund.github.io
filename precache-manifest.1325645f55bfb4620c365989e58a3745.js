35d888669ab21c30ae4c29e9dfe0ba1d476ebbea
Hugh Bromund <hughbromund44@gmail.com>
2020-05-24 20:48:00 -0400

    Updates

---
+++ precache-manifest.1325645f55bfb4620c365989e58a3745.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737240021ceaa404bffdf93a69c9f891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f1d4f36f859c552a2c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a49030df2684361140b2",
+    "url": "/static/css/main.a2919ff5.chunk.css"
+  },
+  {
+    "revision": "e4f1d4f36f859c552a2c",
+    "url": "/static/js/2.7cd8c4d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7cd8c4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49030df2684361140b2",
+    "url": "/static/js/main.a2690c8a.chunk.js"
+  },
+  {
+    "revision": "f1b8cb8ba2ed8ff3eb19",
+    "url": "/static/js/runtime-main.58c4d41b.js"
+  },
+  {
+    "revision": "8469af58339c5e62859743e1d4780db0",
+    "url": "/static/media/background.8469af58.jpg"
+  },
+  {
+    "revision": "20f5f96dccdf200ca1ee949d1012afa3",
+    "url": "/static/media/finex_logo.20f5f96d.jpg"
+  },
+  {
+    "revision": "a9098b59d0ed20bbbc924d2b31187863",
+    "url": "/static/media/hugh.a9098b59.JPG"
+  },
+  {
+    "revision": "7424a966cfac5f2bd3c775154f525cc8",
+    "url": "/static/media/sloth_logo.7424a966.jpg"
+  },
+  {
+    "revision": "82073242bc9ed744aa1891b4c141f1b9",
+    "url": "/static/media/starstats.82073242.png"
+  }
+]);
